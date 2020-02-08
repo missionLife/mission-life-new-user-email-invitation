e9743dff29059d374e23e7d89132364a6da40378
missionLife <SSJ6Porfy@hotmail.com>
2020-02-08 18:01:24 -0500

    adding call to reach in mission life new users scheduler

--- src/mission-life-new-users-scheduler/index.js
+++ src/mission-life-new-users-scheduler/index.js
@@ -1,8 +1,18 @@
+import ReachService from './shared/reach-service';
 
+async function getNewUsers(event, context) {
+  const reachService = new ReachService();
+
+  const allSponsorships = await reachService.getAllSponsorships(1, []);
+
+  console.log('######## ALL SPONSORSHIPS ######### - ', allSponsorships);
+
+  return allSponsorships;
+};
 
 exports.handler = async (event, context) => {
   try {
-    return `Successful call. Print env - ${process.env.TEST_ENV} - in the Mission Life New Users Scheduler Lambda`;
+    return await getNewUsers(event, context);
   } catch (error) {
     throw new Error(`An error occurred in the Mission Life New Users Scheduler Lambda: ${error.message}`);
   }
