b81097c74c57d42abc424357bef697fc3ddaca4f
missionLife <SSJ6Porfy@hotmail.com>
2020-03-01 12:05:59 -0500

    new create user test

--- src/mission-life-new-user-email-notification/index.js
+++ src/mission-life-new-user-email-notification/index.js
@@ -49,88 +49,88 @@ async function createCognitoUsers(messages) {
 }
 
 async function sendNewUserEmail(newUsers) {
+  console.log('NEW CREATED USERS: ', newUsers);
+  return Promise.resolve({});
+  // const batchPromises = [];
 
-  const batchPromises = [];
+  // for (let i = 0; i < newUsers.length; i++) {
+  //   const newUser = newUsers[i];
+  //   // Replace sender@example.com with your "From" address.
+  //   // This address must be verified with Amazon SES.
+  //   const sender = "Admin <admin@missionlifechange.org>";
 
-  for (let i = 0; i < newUsers.length; i++) {
-    const newUser = newUsers[i];
-    // Replace sender@example.com with your "From" address.
-    // This address must be verified with Amazon SES.
-    const sender = "Admin <admin@missionlifechange.org>";
+  //   // Replace recipient@example.com with a "To" address. If your account 
+  //   // is still in the sandbox, this address must be verified.
+  //   const recipient = newUser.email;
 
-    // Replace recipient@example.com with a "To" address. If your account 
-    // is still in the sandbox, this address must be verified.
-    const recipient = newUser.email;
+  //   // Specify a configuration set. If you do not want to use a configuration
+  //   // set, comment the following variable, and the 
+  //   // ConfigurationSetName : configuration_set argument below.
+  //   const configuration_set = "ConfigSet";
 
-    // Specify a configuration set. If you do not want to use a configuration
-    // set, comment the following variable, and the 
-    // ConfigurationSetName : configuration_set argument below.
-    const configuration_set = "ConfigSet";
+  //   // The subject line for the email.
+  //   const subject = "Welcome to Mission Life";
 
-    // The subject line for the email.
-    const subject = "Welcome to Mission Life";
-
-    // The email body for recipients with non-HTML email clients.
-    const body_text = "Amazon SES Test (SDK for JavaScript in Node.js)\r\n"
-                    + "This email was sent with Amazon SES using the "
-                    + "AWS SDK for JavaScript in Node.js.";
+  //   // The email body for recipients with non-HTML email clients.
+  //   const body_text = "Amazon SES Test (SDK for JavaScript in Node.js)\r\n"
+  //                   + "This email was sent with Amazon SES using the "
+  //                   + "AWS SDK for JavaScript in Node.js.";
                 
-    // The HTML body of the email.
-    const body_html = `<html>
-    <head></head>
-    <body>
-      <h1>Amazon SES Test (SDK for JavaScript in Node.js)</h1>
-      <p>This email was sent with
-        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-        <a href='https://aws.amazon.com/sdk-for-node-js/'>
-          AWS SDK for JavaScript in Node.js</a>.</p>
-    </body>
-    </html>`;
+  //   // The HTML body of the email.
+  //   const body_html = `<html>
+  //   <head></head>
+  //   <body>
+  //     <h1>Amazon SES Test (SDK for JavaScript in Node.js)</h1>
+  //     <p>This email was sent with
+  //       <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+  //       <a href='https://aws.amazon.com/sdk-for-node-js/'>
+  //         AWS SDK for JavaScript in Node.js</a>.</p>
+  //   </body>
+  //   </html>`;
 
-    // The character encoding for the email.
-    const charset = "UTF-8";
+  //   // The character encoding for the email.
+  //   const charset = "UTF-8";
 
-    // Create a new SES object. 
-    var ses = new aws.SES();
+  //   // Create a new SES object. 
+  //   var ses = new aws.SES();
 
-    // Specify the parameters to pass to the API.
-    var params = { 
-      Source: sender, 
-      Destination: { 
-        ToAddresses: [
-          recipient 
-        ],
-      },
-      Message: {
-        Subject: {
-          Data: subject,
-          Charset: charset
-        },
-        Body: {
-          Text: {
-            Data: body_text,
-            Charset: charset 
-          },
-          Html: {
-            Data: body_html,
-            Charset: charset
-          }
-        }
-      },
-      ConfigurationSetName: configuration_set
-    };
+  //   // Specify the parameters to pass to the API.
+  //   var params = { 
+  //     Source: sender, 
+  //     Destination: { 
+  //       ToAddresses: [
+  //         recipient 
+  //       ],
+  //     },
+  //     Message: {
+  //       Subject: {
+  //         Data: subject,
+  //         Charset: charset
+  //       },
+  //       Body: {
+  //         Text: {
+  //           Data: body_text,
+  //           Charset: charset 
+  //         },
+  //         Html: {
+  //           Data: body_html,
+  //           Charset: charset
+  //         }
+  //       }
+  //     },
+  //     ConfigurationSetName: configuration_set
+  //   };
 
-    //Try to send the email.
-    batchPromises.push(ses.sendEmail(params).promise());
-  }
-  return Promise.all(batchPromises);
+  //   //Try to send the email.
+  //   batchPromises.push(ses.sendEmail(params).promise());
+  // }
+  // return Promise.all(batchPromises);
 }
 
 async function consume(event, context) {
-  // const cognitoCreatedUsers = await createCognitoUsers(event.Records);
+  const cognitoCreatedUsers = await createCognitoUsers(event.Records);
 
-  // return sendNewUserEmail();
-  return createCognitoUsers(event.Records);
+  return sendNewUserEmail(cognitoCreatedUsers);
 };
 exports.handler = async (event, context) => {
   try {
